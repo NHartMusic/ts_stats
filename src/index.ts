006cce837be14d73587403619f0076a108a9be85
Nick Hart <nhartmusic@gmail.com>
2020-07-12 19:06:27 +0200

    enum set up

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,29 @@ const matches = fs.readFileSync('football.csv', {
         return row.split(',')
     })
 
-console.log(matches)
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+}
+
+
+const printMatchResult = (): MatchResult => {
+    if (match[5] === 'H') {
+        return MatchResult.HomeWin
+    }
+
+    return MatchResult.AwayWin
+}
+
+let manUnitedWins = 0
+
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++
+    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++
+    }
+}
+
+console.log(`Man United won: ${manUnitedWins} games`)
