5a1334d57c5abe51b6c372552231ca5826115258
Nick Hart <nhartmusic@gmail.com>
2020-07-15 13:20:33 +0200

    creating HTML reports

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
 import { MatchReader } from './MatchReader'
 import { CSVFileReader } from './CSVFileReader'
-import { MatchResult } from './MatchResult'
+import { ConsoleReport } from './reportTargets/ConsoleReports'
+import { WinsAnalysis } from './analysers/WinsAnalysis'
+import { Summary } from './Summary'
+import { HTMLReport } from './reportTargets/HTMLReports'
 
 const csvFileReader = new CSVFileReader('football.csv')
 
 const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
 
-let manUnitedWins = 0
+const summary = Summary.winsAnalysisWithHtmlReport('Man United')
 
-for (let match of matchReader.matches) {
-    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-        manUnitedWins++
-    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-        manUnitedWins++
-    }
-}
-
-console.log(`Man United won: ${manUnitedWins} games`)
+summary.buildAndPrintReport(matchReader.matches)
